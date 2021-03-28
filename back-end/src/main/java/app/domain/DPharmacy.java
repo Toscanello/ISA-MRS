48d59aa4fe4b89c778a20b7e77fab23d93de6722
Jovana <klimentaj99@gmail.com>
2021-03-28 13:12:14 +0200

    Mapping classes to entities - address, location.

--- back-end/src/main/java/app/domain/DPharmacy.java
+++ back-end/src/main/java/app/domain/DPharmacy.java
@@ -1,20 +1,38 @@
 package app.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class DPharmacy {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long regNo;  /* Registry numberm - unique ID */
+
     @Column(name = "name", nullable = false)
     private String name;
 
+
+    @OneToOne( fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "pharmacy_address", joinColumns = @JoinColumn(name = "pharmacy_id", referencedColumnName = "regNo"), inverseJoinColumns = @JoinColumn(name = "address_id", referencedColumnName = "id"))
+    @NotNull
+    private Address address;
+
     public DPharmacy() { }
 
-    public DPharmacy(String name) {
+    public DPharmacy(String name, Address address) {
         this.name = name;
+        this.address = address;
     }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
     public String getName() {
         return name;
     }
