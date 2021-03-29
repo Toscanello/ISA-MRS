4d5f0280073834a3bc2a14d42cbf7fe563dad03f
matija <matija.matovic@uns.ac.rs>
2021-03-29 11:36:49 +0200

    Added functions to fetch pharmacy from DB by the pharmacy's registry number.

--- back-end/src/main/java/app/controller/PharmacyController.java
+++ back-end/src/main/java/app/controller/PharmacyController.java
@@ -1,14 +1,13 @@
 package app.controller;
 
 import app.domain.DPharmacy;
+import app.domain.Pharmacy;
 import app.service.PharmacyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,10 +17,17 @@ import java.util.List;
 public class PharmacyController {
     @Autowired
     private PharmacyService pharmacyService;
+
     @GetMapping(value = "/all")
-    public ResponseEntity<List<DPharmacy>> getAllStudents() {
-        List<DPharmacy> pharmacies = pharmacyService.findAll();
-        int s = 0;
+    public ResponseEntity<List<Pharmacy>> getAllStudents() {
+        List<Pharmacy> pharmacies = pharmacyService.findAll();
         return new ResponseEntity<>(pharmacies, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{regNo}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Pharmacy> getPharmacy(@PathVariable String regNo) {
+        Pharmacy p = pharmacyService.getPharmacy(regNo);
+        HttpStatus status = p == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+        return new ResponseEntity<Pharmacy>(p, status);
     }
 }
--- back-end/src/main/java/app/repository/PharmacyRepository.java
+++ back-end/src/main/java/app/repository/PharmacyRepository.java
@@ -1,9 +1,15 @@
 package app.repository;
 import app.domain.DPharmacy;
+import app.domain.Pharmacy;
+import jdk.internal.jline.internal.Nullable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PharmacyRepository extends JpaRepository<DPharmacy, Long> {
-    public Page<DPharmacy> findAll(Pageable pageable);
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+    public Page<Pharmacy> findAll(Pageable pageable);
+
+    @Nullable
+    public Pharmacy findPharmacyByRegNo(String regNo);
 }
