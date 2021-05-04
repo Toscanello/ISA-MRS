3f945df1362ae2250565f6fd1acef2f2de99e844
Jovana <klimentaj99@gmail.com>
2021-05-04 11:49:53 +0200

    Merge branch 'feature_patients_account' into main

---
+++ front-end/src/components/patient/PharmacistSort.vue
@@ -0,0 +1,25 @@
+<template>
+      <div>
+          Sort
+        <v-combobox @change="sorting()" v-model="select" :items="items" style="margin: 0;
+  padding: 0;"></v-combobox>
+      </div>
+  
+</template>
+
+<script>
+export default {
+  data: () => ({
+    select: "RatingAsc",
+    items: ["RatingAsc", "RatingDesc"],
+  }),
+  methods: {
+    sorting: function () {
+        console.log(this.select)
+      this.$emit("clicked", this.select);
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
