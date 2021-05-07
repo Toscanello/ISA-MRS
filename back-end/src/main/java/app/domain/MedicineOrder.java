5caf16152e162af6479d726a2ea00fcc77fc2352
Jovana <klimentaj99@gmail.com>
2021-05-07 18:57:15 +0200

    Added class MedicineOrder and MedicineOrderList

---
+++ back-end/src/main/java/app/domain/MedicineOrder.java
@@ -0,0 +1,77 @@
+package app.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "medicine_order")
+public class MedicineOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Integer id;
+
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "medicine")
+    @NotNull
+    private Medicine medicine;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "order",referencedColumnName = "id",nullable = false)
+    @JsonIgnore
+    private MedicineOrderList medicineOrderList;
+
+    public MedicineOrder() {
+    }
+
+    public MedicineOrder(Medicine medicine, Integer quantity, Double price) {
+        this.medicine = medicine;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public MedicineOrderList getMedicineOrderList() {
+        return medicineOrderList;
+    }
+
+    public void setMedicineOrderList(MedicineOrderList medicineOrderList) {
+        this.medicineOrderList = medicineOrderList;
+    }
+
+    public Medicine getMedicine() {
+        return medicine;
+    }
+
+    public void setMedicine(Medicine medicine) {
+        this.medicine = medicine;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
