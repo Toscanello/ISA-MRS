c5b008d117384c3432b5fef4e56ea8268928c3b8
matija <matija.matovic99@gmail.com>
2021-05-21 22:20:14 +0200

    Finished accepting/declining supplier offers

---
+++ front-end/src/views/bulkOrder/OffersList.vue
@@ -0,0 +1,72 @@
+<template>
+    <div>
+        <pharmacy-admin-home></pharmacy-admin-home>
+        <v-row justify="center">
+            <v-col cols="12" sm="10" md="8" lg="8">
+                <v-data-table
+                    :headers="headers"
+                    :items="offers"
+                    :items-per-page="10"
+                    class="elevation-1"
+                >
+                    <template v-slot:item.actions="{ item }">
+                        <v-icon small class="mr-2" @click="acceptOffer(item)">
+                            mdi-send-check
+                        </v-icon>
+                    </template>
+                </v-data-table>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import PharmacyAdminHome from '../PharmacyAdminHome.vue'
+export default {
+  components: { PharmacyAdminHome },
+  name: 'OffersList',
+  data() {
+      return {
+          offers: [],
+          headers: [
+              {text:'Supplier', value: 'supplier'},
+              {text: 'Price', value: 'price'},
+              {text: 'Available on', value: 'availableOnDate'},
+              {text: 'Status', value: 'status'},
+              {text: 'Accept', value: 'actions'}
+          ]
+      }
+  },
+  mounted() {
+      let path = 'http://localhost:9090/api/order/offers/' + this.$route.params.id
+      axios
+      .get(path)
+      .then(response => {
+          this.offers = response.data
+      })
+      .catch(error => {
+          alert(error.response.data)
+      })
+  },
+  methods: {
+      acceptOffer(item) {
+          let path = 'http://localhost:9090/api/order/offer/accept/' + item.order_id + '/' + item.id
+          axios
+          .put(path)
+          .then(response => {
+              console.log(response.data)
+              alert('Succesfully accepted order')
+              this.$router.go()
+          })
+          .catch(error => {
+              alert(error.response.status)
+          })
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ front-end/src/views/bulkOrder/OrderList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+      <pharmacy-admin-home></pharmacy-admin-home>
+      <v-row justify="center">
+          <v-col cols="12" sm="10" md="8" lg="8">
+              <v-data-table
+                :headers="headers"
+                :items="orders"
+                :items-per-page="10"
+                :expanded.sync="expanded"
+                show-expand
+                @click:row = "viewOffers"
+                class="elevation-1"
+              >
+                <template v-slot:expanded-item="{ headers, item }">
+                    <td :colspan="headers.length">
+                    <v-simple-table>
+                        <template v-slot:default>
+                            <tbody>
+                                <tr v-for="orderItem in item.bulkOrderItemList" 
+                                  :key="orderItem.medicineCode">
+                                    <td>{{orderItem.medicineName}}</td>
+                                    <td>Amount: {{orderItem.amount}}</td>
+                                </tr>
+                            </tbody>
+                        </template>
+                    </v-simple-table>
+                    </td>
+                </template>
+              </v-data-table>
+          </v-col>
+      </v-row>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import PharmacyAdminHome from '../PharmacyAdminHome.vue'
+export default {
+  components: { PharmacyAdminHome },
+    name: 'OrdersList',
+    data() {
+        return {
+            orders: [],
+            headers: [
+                {text: 'Order ID', value: 'id'},
+                {text: 'Due date', value: 'dueDate'},
+            ],
+            expanded: [],
+        }
+    },
+    mounted() {
+        let path = 'http://localhost:9090/api/order/all/' + this.$route.params.regNo
+        axios
+        .get(path)
+        .then(response => {
+            this.orders = response.data
+        })
+        .catch(error => {
+            alert(error.response.data)
+        })
+    },
+    methods: {
+        viewOffers(data) {
+            this.$router.push('/offers/' + data.id)
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
