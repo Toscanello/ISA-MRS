60bbc328acd3f77125371c4565c35a47a53ed36a
matija <matija.matovic99@gmail.com>
2021-05-22 12:54:28 +0200

    Added discount class

---
+++ back-end/src/main/java/app/domain/Discount.java
@@ -0,0 +1,76 @@
+package app.domain;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+public class Discount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "medicine_code", referencedColumnName = "code")
+    private Medicine medicine;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Pharmacy pharmacy;
+
+    @Column(name = "percentage")
+    private double percentage;
+
+    @Column(name = "start_date")
+    private LocalDate startDate;
+
+    @Column(name = "end_date")
+    private LocalDate endDate;
+
+    public Discount() { }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Medicine getMedicine() {
+        return medicine;
+    }
+
+    public void setMedicine(Medicine medicine) {
+        this.medicine = medicine;
+    }
+
+    public Pharmacy getPharmacy() {
+        return pharmacy;
+    }
+
+    public void setPharmacy(Pharmacy pharmacy) {
+        this.pharmacy = pharmacy;
+    }
+
+    public double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(double percentage) {
+        this.percentage = percentage;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
